947753188ecbdcd73f37ce34acd2463d30641ce8
Maiky Perlee <maiky-perlee@outlook.com>
2022-11-02 10:57:36 +0100

    implemented start quiz page + finish page with chart

--- src/Finished.tsx
+++ src/Finished.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import { selectAnswers } from "./redux/slices/answersSlice";
+import { useAppSelector } from "./redux/store";
+
+import { Bar } from "react-chartjs-2";
+import {
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  registry,
+} from "chart.js";
+import { questions } from "./data/questions";
+import { Navigate } from "react-router-dom";
+
+registry.addElements(PointElement, BarElement);
+registry.addScales(CategoryScale, LinearScale);
 
 type Props = {};
 
 const Finished = (props: Props) => {
-  return <div>Finished</div>;
+  const answers = useAppSelector(selectAnswers);
+
+  if (answers.length !== questions.length) {
+    return <Navigate to={`/vragen/${answers.length + 1}`} />;
+  }
+
+  return (
+    <div className="h-screen w-screen bg-gradient-to-r from-cyan-400 to-cyan-600 flex justify-center items-center">
+      <div className="bg-white shadow-md p-5 w-1/2 rounded-md">
+        <Bar
+          datasetIdKey="id"
+          data={{
+            labels: answers.map((answer) => `Vraag #${answer.questionNo}`),
+            datasets: [
+              {
+                data: answers.map((answer) => answer.answer),
+              },
+            ],
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Finished;
--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Welcome = (props: Props) => {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen w-screen bg-gradient-to-r from-cyan-400 to-cyan-600 flex justify-center items-center">
+      <button
+        onClick={() => navigate("/vragen/1")}
+        className="px-10 py-5 bg-slate-700 text-white rounded-md text-2xl font-semibold"
+      >
+        Start vragenlijst
+      </button>
+    </div>
+  );
 };
 
 export default Welcome;
